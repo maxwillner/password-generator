20defe35b7ef3d878a88be0b508d240fba477d5c
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-09-14 01:07:12 -0400

    added for-loop to iterate over selected potential password characters for number of characters selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 // Assignment code here
 var numberOfCharacters;
-var uppercaseConfirm;
 var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercaseConfirm;
-var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var specialCharacterConfirm;
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-var numericConfirm;
-var numericValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var numericValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var availableCharacters = [];
+var password = [];
 
 var passwordInput;
 var createPassword;
+var index;
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -18,146 +18,91 @@ var generateBtn = document.querySelector("#generate");
 // // Add event listener to generate button
 generateBtn.addEventListener("click", function() {
   
-  // debugger;
+  debugger;
   alert("Generate Your Password Now!");
-  getUserInputs()
-  passwordInput()
-  generatePassword()
+  generatePassword();
 });
+
+// addArray(uppercaseLetters, lowercaseLetters, specialCharacters, numericValues);
+// console.log(availableCharacters)
 
 // User Inputs
 function getUserInputs() {
-
   // Determine how many characters user wants to use
-  var numberOfCharacters =
-  prompt("How many characters do you want in your password? Please choose between 8 and 128 characters.")
+  numberOfCharacters = prompt("How many characters do you want in your password? Please choose between 8 and 128 characters.")
   if (isNaN(numberOfCharacters)) {
-    alert("Please enter a number!")
-  }
+    alert("Please enter a number!");
+    getUserInputs();
+  };
   if (numberOfCharacters < 8 || numberOfCharacters > 128) {
     alert("Please choose between 8 and 128 characters.");
-    getUserInputs()
-  } 
-}
+    getUserInputs();
+  }; 
+};
+
 
 
 // Generate Password
 function passwordInput() {
   // Confirm if user wants uppercase characters
-  var uppercaseConfirm =
-  prompt("Do you want to include uppercase letters in your password?")
-  if (uppercaseConfirm === "yes") {
-    passwordInput = uppercaseLetters
+  var uppercaseConfirm = confirm("Do you want to include uppercase letters in your password?");
+  if (uppercaseConfirm) {
+    availableCharacters.push(...uppercaseLetters);
   }
   else {
-    alert("You have chosen to exclude uppercase letters in your password!")
+    alert("You have chosen to exclude uppercase letters in your password!");
   };
 
   // Confirm if user wants lowercase characters
-  var lowercaseConfirm =
-  prompt("Do you want to include lowercase letters in your password?")
-  if (uppercaseConfirm === "yes" && lowercaseConfirm === "yes") {
-    passwordInput = uppercaseLetters.concat(lowercaseLetters)
-  } else if (uppercaseConfirm === "no" && lowercaseConfirm === "yes") {
-    passwordInput = lowercaseLetters
-  }
-  else {
-    alert("You have chosen to exclude lowercase letters in your password!")
+  var lowercaseConfirm = confirm("Do you want to include lowercase letters in your password?");
+  if (lowercaseConfirm) {
+    availableCharacters.push(...lowercaseLetters);
+  } else {
+    alert("You have chosen to exclude lowercase letters in your password!");
   }
 
   // Confirm if user wants special charaters 
-  var specialCharacterConfirm = 
-  prompt("Do you want to include special characters in your password?")
-  if (uppercaseConfirm === "yes" && lowercaseConfirm === "yes" && specialCharacterConfirm === "yes") {
-    passwordInput = uppercaseLetters.concat(lowercaseLetters).concat(specialCharacters);
-  }
-  else if (uppercaseConfirm === "yes" && lowercaseConfirm === "no" && specialCharacterConfirm === "yes") {
-    passwordInput = uppercaseLetters.concat(specialCharacters);
-  }
-  else if (uppercaseConfirm === "no" && lowercaseConfirm === "yes" && specialCharacterConfirm === "yes") {
-    passwordInput = lowercaseLetters.concat(specialCharacters);
-  }
-  else if (uppercaseConfirm === "no" && lowercaseConfirm === "no" && specialCharacterConfirm === "yes") {
-    passwordInput = specialCharacters;
-  }
-  //else if (uppercaseConfirm === "yes" && lowercaseConfirm === "yes" && special)
-  else {
+  var specialCharacterConfirm = confirm("Do you want to include special characters in your password?")
+  if (specialCharacterConfirm) {
+    availableCharacters.push(...specialCharacters);
+  } else {
     alert("You have chosen to exclude special characters in your password!")
   }
 
   // Confirm if user wants numeric values
-  var numericConfirm =
-  prompt("Do you want to include numeric values in your password?")
-  if (uppercaseConfirm === "yes" && lowercaseConfirm === "yes" && specialCharacterConfirm === "yes" && numericConfirm === "yes") {
-    passwordInput = uppercaseLetters.concat(lowercaseLetters).concat(specialCharacters).concat(numericValues);
-  }
-  else if (uppercaseConfirm === "yes" && lowercaseConfirm === "yes" && specialCharacterConfirm === "no" && numericConfirm === "yes") {
-    passwordInput = uppercaseLetters.concat(lowercaseLetters).concat(numericValues);
-  }
-  else if (uppercaseConfirm === "yes" && lowercaseConfirm === "no" && specialCharacterConfirm === "yes" && numericConfirm === "yes") {
-    passwordInput = uppercaseLetters.concat(specialCharacters).concat(numericValues);
-  }
-  else if (uppercaseConfirm === "no" && lowercaseConfirm === "yes" && specialCharacterConfirm === "yes" && numericConfirm === "yes") {
-    passwordInput = lowercaseLetters.concat(specialCharacters).concat(numericValues);
-  }
-  else if (uppercaseConfirm == "yes" && lowercaseConfirm === "yes" && specialCharacterConfirm === "yes" && numericConfirm === "no") {
-    alert("You have chosen to exclude numeric values in your password!")
-    passwordInput = uppercaseLetters.concat(lowercaseLetters).concat(specialCharacters);
-  }
-  else if (uppercaseConfirm === "yes" && lowercaseConfirm === "no" && specialCharacterConfirm === "no" && numericConfirm === "yes") {
-    passwordInput = uppercaseLetters.concat(numericValues);
-  }
-  else if (uppercaseConfirm === "no" && lowercaseConfirm === "yes" && specialCharacterConfirm === "no" && numericConfirm === "yes") {
-    passwordInput = lowercaseLetters.concat(numericValues);
-  }
-  else if (uppercaseConfirm === "no" && lowercaseConfirm === "no" && specialCharacterConfirm === "yes" && numericConfirm === "yes") {
-    passwordInput = specialCharacters.concat(numericValues);
-  }
-  else if (uppercaseConfirm === "no" && lowercaseConfirm === "no" && specialCharacterConfirm === "no" && numericConfirm === "yes") {
-    passwordInput = numericValues;
-  }
-  else {
+  var numericConfirm =  confirm("Do you want to include numeric values in your password?")
+  if (numericConfirm) {
+    availableCharacters.push(...numericValues);
+  } else {
     alert("You have chosen to exclude special characters in your password!")
-  }
+  }; 
 
+  // now have mixture of uppercase, lowercase, specialcharacters, and numericvalues within 'availablecharacters' variable -- 10 - 72 characters
+  // console.log(availableCharacters);
 };
-// // currently stuck!
 
-function generatePassword() {}
-//   var createPassword = [Math.floor(Math.random() * passwordInput.length)]
-//   console.log(createPassword);
+// create password values by looping over 'availablecharacter' array for the 'numberOfCharacters' chosen
+function createPasswordValues() {
+  for (var i = 0; i < numberOfCharacters; i++) {
+    var index = availableCharacters[Math.floor(Math.random() * availableCharacters.length)];
+    
+    // stuck again -- trying to display all password values at once
+    var completePassword = []
+    completePassword.push(...index);
+  };
+};
 
-//   for (var i = 0; i < numberOfCharacters; i++) {
-//     console.log(createPassword[i]);
-  
+// stuckness ends
 
-//   };
+function generatePassword() {  
+  getUserInputs();
+  passwordInput();
+  createPasswordValues();
+};
 
 
-// end stuckness
-
-
-// conditionals -- if yes push into array, if not just don't push
-// Array.concat
-// Math.random * number of characters chosen from Array
-// loop for number of times chose for # of characters
 // show final product
 // ensure all characters 
-
-
-//passwordInput()
-
-
-
-
-// function init() {
-//   getUserInput()
-//   passwordInput()
-//   renderPassword()
-// 
-
-
-
 
 
 // Write password to the #password input
@@ -169,9 +114,3 @@ function generatePassword() {}
 
 // }
 
-// // Add event listener to generate button
-//generateBtn.addEventListener("click", passwordInput);
-// var characterLength = function() {
-//   var character = "";
-
-//   console.log("You have chosen" + character + "characters for your password");
